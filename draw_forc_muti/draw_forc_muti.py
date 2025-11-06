bc18a7cf83c6a25676ff3c6e56042d41f3e5953e
HU MAO RONG <r14224114@ntu.edu.tw>
2025-11-06 18:15:27 +0800

    modified draw_forc_muti

--- draw_forc_muti/draw_forc_muti.py
+++ draw_forc_muti/draw_forc_muti.py
@@ -5,73 +5,72 @@ import matplotlib.pyplot as plt
 
 
 "writing file"
-#forc_GRm11_r01 = np.load('GRm11_r01_forc.npy')
+forc_GRm11_r01 = np.load('GRm11_r01_forc.npy')
 forc_GRm12_r02 = np.load('GRm12_r02_forc.npy')
-#forc_GRm13_r03 = np.load('GRm13_r03_forc.npy')
+forc_GRm13_r03 = np.load('GRm13_r03_forc.npy')
 
 
-"extract for foc_np array"
-#GRm11_time = forc_GRm11_r01[:,1]
-#GRm12_time = forc_GRm12_r02[:,1]
-#GRm13_time = forc_GRm13_r03[:,1]
+
 
 
 "GRm11"
-#GRm11_force_l = forc_GRm11_r01[:,2]  
-#GRm11_force_r = forc_GRm11_r01[:,3]
+GRm11_time_forc= forc_GRm11_r01[:,[1,3]]
 GRm12_time_forc= forc_GRm12_r02[:,[1,3]]
+GRm13_time_forc= forc_GRm13_r03[:,[1,3]]
 
 "extract time from GRm**_time, with fixed threshold(t = 0.01Myr)"
 #inorder to compair different model with the same timestep
 #because the size of the time array of orginal data is different for each mode
 threshold = float(0.01) #10 kyr
-current_time = GRm12_time_forc[0,0]
-time_forc_r = []
+
+GRm11_current_time = GRm11_time_forc[0,0]
+GRm12_current_time = GRm12_time_forc[0,0]
+GRm13_current_time = GRm13_time_forc[0,0]
+
+GRm11_time_forc_r = []
+GRm12_time_forc_r = []
+GRm13_time_forc_r = []
 
 "getting 1st vaule > threshold"
-for row in GRm12_time_forc:
+for row in GRm11_time_forc:
     
-    if row[0] >= current_time:
+    if row[0] >= GRm11_current_time:
         
         time_round =np.round(row[0]/threshold) * threshold
-        time_forc_r.append([time_round,row[1]])    
-        current_time = current_time + threshold 
-
-time_forc_r = np.array(time_forc_r, dtype = float)  #this code can be written in a cleaner fashio        
-
+        GRm11_time_forc_r.append([time_round,row[1]])    
+        GRm11_current_time = GRm11_current_time + threshold 
         
+for row in GRm12_time_forc:
+    
+    if row[0] >= GRm12_current_time:
+        
+        time_round =np.round(row[0]/threshold) * threshold
+        GRm12_time_forc_r.append([time_round,row[1]])    
+        GRm12_current_time = GRm12_current_time + threshold 
+        
+for row in GRm13_time_forc:
+    
+    if row[0] >= GRm13_current_time:
+        
+        time_round =np.round(row[0]/threshold) * threshold
+        GRm13_time_forc_r.append([time_round,row[1]])    
+        GRm13_current_time = GRm13_current_time + threshold 
+
+
+GRm11_time_forc_r = np.array(GRm11_time_forc_r, dtype = float)  #this code can be written in a cleaner fashio        
+GRm12_time_forc_r = np.array(GRm12_time_forc_r, dtype = float)
+GRm13_time_forc_r = np.array(GRm13_time_forc_r, dtype = float)        
          
-    
-     
-    
-
-"GRm12"
-#GRm12_force_l = forc_GRm12_r02[:,2]  
-#GRm12_force_r = forc_GRm12_r02[:,3]
-
-"GRm13" 
-#GRm13_force_l = forc_GRm13_r03[:,2]  
-#GRm13_force_r = forc_GRm13_r03[:,3]
-
 "GRm11"
-#plt.plot(time, GRm11_force_l, label='GRm11 Left', linestyle='-', color='blue')
-#plt.plot(GRm11_time, GRm11_force_r, label='GRm11 Right', linestyle='--', color='blue')
 
+plt.plot(GRm11_time_forc_r[:,0],GRm11_time_forc_r[:,1], color='r')
 "GRm12"
-#plt.plot(time, GRm12_force_l, label='GRm12 Left', linestyle='-', color='red')
-plt.plot(time_forc_r[:,0],time_forc_r[:,1], color='b')
 
-
+plt.plot(GRm12_time_forc_r[:,0],GRm12_time_forc_r[:,1], color='b')
 
 "GRm13"
-#plt.plot(time, GRm13_force_l, label='GRm13 Left', linestyle='-', color='green')
-#plt.plot(time, GRm13_force_r, label='GRm13 Right', linestyle='--', color='green')
+plt.plot(GRm13_time_forc_r[:,0],GRm13_time_forc_r[:,1], color='g')
 
-# --- Styling ---
-#plt.xlabel('Time')
-#plt.ylabel('Force')
-#plt.title('Right Force vs Time (GRm11–GRm13)')
-#plt.legend()
-#plt.grid(True)
-#plt.tight_layout()
-#plt.show()
+
+
+
