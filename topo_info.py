cb8f2e6ac8b5b5d8f93cb286e49f6493d84e0513
HU MAO RONG <r14224114@ntu.edu.tw>
2025-11-13 09:48:19 +0800

    changed top_info, so it can be run on 301PC and other PC

--- topo_info.py
+++ topo_info.py
@@ -1,5 +1,5 @@
 "topo information"
-"last updated in 02/03/2024"
+"last updated in 11/13/2025"
 #this file must exit in the file of targeted folder
 #to draw topo, _contents.0, nxnz.o, mesh.o, flac.vts are required
 
@@ -9,7 +9,7 @@
 import sys
 import math
 import os
-sys.path.append("C:/Users/carosn/Desktop/flac.py") #fdirect path of flac
+sys.path.append("C:/Users/admin/Desktop/model_download_file/flac.py") #fdirect path of flac, path depends which PC your on
 import flac
 fl=flac.Flac()
 import numpy as np
@@ -26,7 +26,7 @@ def topo_spf(x,z):
     plt.xlabel("width(km)")
     plt.ylabel("depth(km)")
     plt.plot(dsx, rlf)
-    plt.savefig(f"C:/Users/carosn/Desktop/model_220/topo_initial", dpi = 300)
+#    plt.savefig(f"C:/Users/carosn/Desktop/model_220/topo_initial", dpi = 300)
 
     return 
 def topo_chng(x,z):
@@ -43,7 +43,7 @@ def topo_chng(x,z):
         plt.xlabel("width(km)")
         plt.ylabel("depth(km)")
         plt.plot(dsx, rlf)
-        plt.savefig(f'C:/Users/carosn/Desktop/model_220/timestep/time_step_{i}', dpi = 300)
+#        plt.savefig(f'C:/Users/carosn/Desktop/model_220/timestep/time_step_{i}', dpi = 300)
         plt.clf()  #clear the old plot
 
     return
@@ -52,7 +52,7 @@ def topo_local(x,z):
     lths = int(input('from which time_step?')) #lenth start
     lthe = int(input('to which time_step?')) #lenth end
     xs = int(input('from where?(node)')) #x start
-    xe = int(input('to where?(node)'))    #x end   
+    xe = int(input('to where?(node)'))    #x end       
     for i in range(lths, lthe+1):
         
         x,z = fl.read_mesh(i) 
@@ -64,6 +64,7 @@ def topo_local(x,z):
         vrdis = abs(lcmx-lcmi) #vertical displacement of from the lowest point to the height
         indxmx = np.argmax(lcrlf)
         indxmi = np.argmin(lcrlf)
+        print(lcrlf,lcdsx)
         
         print("trench to heightest point = ",round(vrdis*1000), "(m)")
         
@@ -71,11 +72,12 @@ def topo_local(x,z):
         plt.xlabel("width(km)")
         plt.ylabel("depth(km)")
         plt.plot(lcdsx, lcrlf)
+        plt.ylim(-10, 10) #limit range of relief to -10km ~ 10km
         plt.scatter(lcdsx[indxmx],lcmx, label ='max') #show local maximun
         plt.scatter(lcdsx[indxmi],lcmi, label = 'min')#show local minimun
         plt.text(lcdsx[indxmx],lcmx, f'{lcmx}(km)',)
         plt.text(lcdsx[indxmi],lcmi,f'{lcmi}(km)')
-        plt.savefig(f'C:/Users/carosn/Desktop/model_220/local_relief/local_relief_time_step_{i}', dpi = 300)
+        plt.savefig(f'C:/Users/admin/Desktop/model_download_file/GRm11_r01/figure/local_relief/local_relief_time_step_{i}', dpi = 300) #save in the target folder 
         plt.clf()  #clear old plot 
     return    
 
@@ -88,7 +90,8 @@ nz = nxnz[1] #number of node for z
 print("number of node")
 print("nx = ",nx,"nz = ",nz)
 
-
+"draw topo"
+#delete #, for the topo that want to be runned
 #change in topopraphy with each time step(total timestep)
 #topo_chng(x,z)
 #topo for specific
