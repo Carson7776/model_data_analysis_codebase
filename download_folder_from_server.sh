d452f52270528767dce3a31dd5338fc3deb7d90a
HU MAO RONG <r14224114@ntu.edu.tw>
2025-11-21 16:01:08 +0800

    Add script to download folder from IES server
    This script downloads a specified folder from the IES server to a local path using SCP.

---
+++ download_folder_from_server.sh
@@ -0,0 +1,24 @@
+# !/bin/bash
+#the following script is to download the entire folder from IES server
+
+#IES server information
+User="humaorong"
+Host="140.109.81.186"
+
+#path
+remote_path="/scratch2/humaorong"
+local_path="/drives/C/Users/carso/model_download_file"
+
+#choose folder to download
+echo "choose folders to download"
+read  -a folder
+
+#run scp to download folder
+for folder in "${folder[@]}"
+do
+        #create folder at local
+        mkdir -p "${local_path}/${folder}"
+        scp -r  ${User}@${Host}:${remote_path}/${folder}  ${local_path}/${folder}
+done
+
+echo "fininsh downloading folder"
